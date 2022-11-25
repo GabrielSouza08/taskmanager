b36e605701299271bc02da2e9286c8f830f9666d
Gabriel Souza Calixto <gabriel15806@gmail.com>
2022-11-25 09:31:12 -0300

    Métodos do controller refatorados para utilizar os novos middlewares

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const app = express();
 const port = 3000;
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
-app.use(express.json());
-app.use(express.static("./public"));
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 require("dotenv").config();
 
+app.use(express.json());
+app.use(express.static("./public"));
+
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
